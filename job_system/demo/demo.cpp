2a674234650636821ce0eaf11e77779e50aeeee2
mathieu-portfolio <mathieu.sterlin+portfolio@gmail.com>
2026-04-21 17:04:08 +0200

    job_system: update demo to showcase execution and dependency scheduling

--- job_system/demo/demo.cpp
+++ job_system/demo/demo.cpp
@@ -1,24 +1,50 @@
 #include "job_system.hpp"
 
 #include <iostream>
+#include <mutex>
 
 int main()
 {
+    std::mutex cout_mutex;
+
     JobSystem jobs(4);
 
-    const JobId a = jobs.create_job([] {
-        std::cout << "job A\n";
+    const JobId a = jobs.create_job([&] {
+        std::lock_guard<std::mutex> lock(cout_mutex);
+        std::cout << "job A: prepare data\n";
     });
 
-    const JobId b = jobs.create_job([] {
-        std::cout << "job B\n";
+    const JobId b = jobs.create_job([&] {
+        std::lock_guard<std::mutex> lock(cout_mutex);
+        std::cout << "job B: independent work\n";
     });
 
-    (void)a;
-    (void)b;
+    const JobId c = jobs.create_job([&] {
+        std::lock_guard<std::mutex> lock(cout_mutex);
+        std::cout << "job C: depends on A\n";
+    });
 
-    std::cout << "Job system skeleton created.\n";
-    std::cout << "Dependencies, run(), and wait() are not implemented yet.\n";
+    const JobId d = jobs.create_job([&] {
+        std::lock_guard<std::mutex> lock(cout_mutex);
+        std::cout << "job D: depends on A and B\n";
+    });
+
+    jobs.add_dependency(c, a);
+    jobs.add_dependency(d, a);
+    jobs.add_dependency(d, b);
+
+    {
+        std::lock_guard<std::mutex> lock(cout_mutex);
+        std::cout << "Running job batch...\n";
+    }
+
+    jobs.run();
+    jobs.wait();
+
+    {
+        std::lock_guard<std::mutex> lock(cout_mutex);
+        std::cout << "All jobs completed.\n";
+    }
 
     return 0;
 }

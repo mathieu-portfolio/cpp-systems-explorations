f0b70c0f2ea6b07d4b4e0203b1cc8913f92104a0
mathieu-portfolio <mathieu.sterlin+portfolio@gmail.com>
2026-04-21 20:21:43 +0200

    task_graph: improve demo to better illustrate dependency flow and parallel execution

--- task_graph/demo/demo.cpp
+++ task_graph/demo/demo.cpp
@@ -1,7 +1,9 @@
 #include "task_graph.hpp"
 
+#include <chrono>
 #include <iostream>
 #include <mutex>
+#include <thread>
 
 int main()
 {
@@ -12,36 +14,62 @@ int main()
         std::cout << msg << '\n';
     };
 
-    std::cout << "Pipeline graph:\n";
-    std::cout << "load -> parse -> transform -> save\n";
-    std::cout << "load -> validate\n\n";
+    std::cout << "Task graph:\n";
+    std::cout << "  load -> parse -> transform ----\\\n";
+    std::cout << "    \\                            -> package -> publish\n";
+    std::cout << "     -> validate ---------------/\n\n";
+
+    std::cout << "Expected behavior:\n";
+    std::cout << "  - load runs first\n";
+    std::cout << "  - parse and validate unlock after load\n";
+    std::cout << "  - transform unlocks after parse\n";
+    std::cout << "  - package waits for both transform and validate\n";
+    std::cout << "  - publish runs last\n\n";
 
     TaskGraph graph(4);
 
     const TaskId load = graph.add_named_task("load", [&] {
+        log("[load] start");
+        std::this_thread::sleep_for(std::chrono::milliseconds(80));
         log("[load] done");
     });
 
     const TaskId parse = graph.add_named_task("parse", [&] {
+        log("[parse] start");
+        std::this_thread::sleep_for(std::chrono::milliseconds(60));
         log("[parse] done");
     });
 
     const TaskId transform = graph.add_named_task("transform", [&] {
+        log("[transform] start");
+        std::this_thread::sleep_for(std::chrono::milliseconds(50));
         log("[transform] done");
     });
 
-    const TaskId save = graph.add_named_task("save", [&] {
-        log("[save] done");
-    });
-
     const TaskId validate = graph.add_named_task("validate", [&] {
+        log("[validate] start");
+        std::this_thread::sleep_for(std::chrono::milliseconds(40));
         log("[validate] done");
     });
 
+    const TaskId package = graph.add_named_task("package", [&] {
+        log("[package] start");
+        std::this_thread::sleep_for(std::chrono::milliseconds(30));
+        log("[package] done");
+    });
+
+    const TaskId publish = graph.add_named_task("publish", [&] {
+        log("[publish] start");
+        std::this_thread::sleep_for(std::chrono::milliseconds(20));
+        log("[publish] done");
+    });
+
     graph.add_edge(load, parse);
-    graph.add_edge(parse, transform);
-    graph.add_edge(transform, save);
     graph.add_edge(load, validate);
+    graph.add_edge(parse, transform);
+    graph.add_edge(transform, package);
+    graph.add_edge(validate, package);
+    graph.add_edge(package, publish);
 
     std::cout << "Running graph...\n\n";
 

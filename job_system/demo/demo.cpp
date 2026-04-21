961f9682793ffc7da094244d1f74075481f39866
mathieu-portfolio <mathieu.sterlin+portfolio@gmail.com>
2026-04-21 17:07:31 +0200

    job_system: improve demo with visual graph and clearer execution phases

--- job_system/demo/demo.cpp
+++ job_system/demo/demo.cpp
@@ -1,50 +1,60 @@
 #include "job_system.hpp"
 
+#include <chrono>
 #include <iostream>
 #include <mutex>
+#include <thread>
 
 int main()
 {
     std::mutex cout_mutex;
 
+    auto log = [&](const char* msg) {
+        std::lock_guard<std::mutex> lock(cout_mutex);
+        std::cout << msg << '\n';
+    };
+
+    std::cout << "Job graph:\n";
+    std::cout << "    A\n";
+    std::cout << "   / \\\n";
+    std::cout << "  C   D\n";
+    std::cout << "   \\ /\n";
+    std::cout << "    B (independent)\n\n";
+
     JobSystem jobs(4);
 
     const JobId a = jobs.create_job([&] {
-        std::lock_guard<std::mutex> lock(cout_mutex);
-        std::cout << "job A: prepare data\n";
+        log("[A] start");
+        std::this_thread::sleep_for(std::chrono::milliseconds(50));
+        log("[A] done");
     });
 
     const JobId b = jobs.create_job([&] {
-        std::lock_guard<std::mutex> lock(cout_mutex);
-        std::cout << "job B: independent work\n";
+        log("[B] start (independent)");
+        std::this_thread::sleep_for(std::chrono::milliseconds(30));
+        log("[B] done");
     });
 
     const JobId c = jobs.create_job([&] {
-        std::lock_guard<std::mutex> lock(cout_mutex);
-        std::cout << "job C: depends on A\n";
+        log("[C] start (after A)");
+        std::this_thread::sleep_for(std::chrono::milliseconds(20));
+        log("[C] done");
     });
 
     const JobId d = jobs.create_job([&] {
-        std::lock_guard<std::mutex> lock(cout_mutex);
-        std::cout << "job D: depends on A and B\n";
+        log("[D] start (after A and B)");
+        std::this_thread::sleep_for(std::chrono::milliseconds(20));
+        log("[D] done");
     });
 
     jobs.add_dependency(c, a);
     jobs.add_dependency(d, a);
     jobs.add_dependency(d, b);
 
-    {
-        std::lock_guard<std::mutex> lock(cout_mutex);
-        std::cout << "Running job batch...\n";
-    }
+    std::cout << "Running job batch...\n\n";
 
     jobs.run();
     jobs.wait();
 
-    {
-        std::lock_guard<std::mutex> lock(cout_mutex);
-        std::cout << "All jobs completed.\n";
-    }
-
-    return 0;
+    std::cout << "\nAll jobs completed.\n";
 }
